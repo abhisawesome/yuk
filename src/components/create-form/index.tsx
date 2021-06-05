632067b91c8395e2baeeaf8bc616f5a6ebd25e79
Abhijith v <abhijithababhijith@gmail.com>
2021-06-06 00:37:10 +0530

    eslint fix

--- src/components/create-form/index.tsx
+++ src/components/create-form/index.tsx
@@ -10,22 +10,31 @@ const CreateForm = () => {
     dispatch({ action: 'add_new_host' });
   };
   // change form element data
-  const onChangeFormElementData = (value:any) => {
+  const onChangeFormElementData = (value: any) => {
     dispatch({ action: 'change_form_data', value });
   };
-  return (<div>
-        <form>
-            {GenerateElement({ value: formElement, onChange: onChangeFormElementData })}
-           <p>
-           <button type="button" onClick={() => { addNewHost(); }}>Add new host</button>
-           </p>
-        </form>
-        <div>
-            <p>
-                Host data: {JSON.stringify(hostData)}
-            </p>
-        </div>
-    </div>);
+  const formSubmit = (event: any) => {
+    event.preventDefault();
+    console.log(hostData);
+  };
+  return (
+    <div>
+      <form>
+        {GenerateElement({ value: formElement, onChange: onChangeFormElementData })}
+        <p>
+          <button type="button" onClick={() => { addNewHost(); }}>Add new host</button>
+        </p>
+        <p>
+          <button type="submit" onClick={formSubmit} >Create</button>
+        </p>
+      </form>
+      <div>
+        <p>
+          Host data: {JSON.stringify(hostData)}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default CreateForm;
