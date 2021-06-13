5d54f2dce6da1c630cb7279fcf96e3f7517defa9
Abhijith v <abhijithababhijith@gmail.com>
2021-06-13 19:54:32 +0530

    Renamed structure

---
+++ src/backend/proxy-settings/settings.ts
@@ -0,0 +1,24 @@
+import NginxLoad from './proxy-configure';
+
+interface HostData {
+    host: String,
+    weight: String,
+    type: String
+}
+interface Input {
+    hostData: Array<HostData>
+}
+
+const configure = async ({ hostData }: Input, location: String) => {
+    try {
+        const loadBalancer = new NginxLoad(hostData);
+        const config = await loadBalancer.createConfig();
+        await loadBalancer.makeConfig(config, location);
+        return Promise.resolve({ status: true, message: 'Config file created' });
+    } catch (error) {
+        console.log(error);
+        return Promise.reject(error);
+    }
+}
+
+export { configure };
---
+++ src/components/Restart-Proxy/restart.tsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import Retry from './retry';
+import Loading from '@/components/loading';
+
+interface Props {
+    isDevelopmentMode?: boolean | true
+}
+
+const RestartProxy = ({ isDevelopmentMode }: Props) => {
+    const [isRestarting, setRestarting] = useState(false);
+    const [message, setMessage] = useState(undefined);
+
+    const restartProxy = async () => {
+        setRestarting(true);
+        try {
+            const response = await fetch('/api/restart');
+            const respJson = await response.json();
+            console.log(respJson);
+            setMessage(respJson.message);
+        } catch (error) {
+            setMessage(error.message || "")
+        }
+        setRestarting(false);
+    }
+    if (isDevelopmentMode) {
+        return (
+            <div className="items-center justify-center p-5 bg-gray-100">
+                Development mode
+            </div>
+        )
+    }
+    if (isRestarting) {
+        return <Loading type="card" />
+    }
+    return (
+        <div>
+            <div className="flex items-center justify-center mt-5">
+                <button className="h-10 w-10" onClick={restartProxy}>
+                    <Retry />
+                </button>
+            </div>
+            <p className="p-5 bg-gray-100">
+                {message}
+            </p>
+        </div>
+    )
+}
+export default RestartProxy
