06113b0dafdeefd966424e89d977ffbfc2fa0fa1
Abhijith v <abhijithababhijith@gmail.com>
2021-06-13 10:05:03 +0530

    wip:tailwind

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -16,6 +16,7 @@ const NavBar = () => {
                         </Link>
                     </div>
                 </div>
+                
             </nav>
         </div>
     )
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -12,6 +12,7 @@ export default () => {
     return (
       <div>
            <NavBar />
+           
       </div>
     )
 }
