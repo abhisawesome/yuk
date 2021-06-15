d786bab124290cd9785228f7b1b3a3397beb181d
Abhijith v <abhijithababhijith@gmail.com>
2021-06-15 09:26:30 +0530

    Added log:backend

--- src/backend/proxy-settings/proxy-configure/create-config/index.ts
+++ src/backend/proxy-settings/proxy-configure/create-config/index.ts
@@ -2,7 +2,8 @@ import createUpstream from './create-upstream';
 import createServer from './create-server';
 import createHttp from './create-http';
 import createEvent from './create-event';
-
+import addAccessLog from './add-access-log';
+import { ACCESS_LOG_LOCATION, ACCESS_LOG_FLUSH_TIME } from '@/constants/index';
 interface HostData {
     host: String,
     weight: String,
@@ -12,7 +13,8 @@ interface HostData {
 const createConfig = (data: Array<HostData>, upstreamName: String, port: Number) => {
     const upstream = createUpstream(data, upstreamName);
     const server = createServer(port, upstreamName);
-    const http = createHttp(upstream, server);
+    const accessLog = addAccessLog(ACCESS_LOG_LOCATION, ACCESS_LOG_FLUSH_TIME)
+    const http = createHttp(upstream, server, accessLog);
     const events = createEvent();
     return Promise.resolve(`${events}\n ${http}`);
 }
